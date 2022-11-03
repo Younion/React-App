49a265172f2b0eb6247684245beb2f600b2ea65a
Younion <106282337+Younion@users.noreply.github.com>
2022-11-02 19:27:29 -0600

    Add files via upload

--- Components/SauceCarousel.js
+++ Components/SauceCarousel.js
@@ -6,9 +6,10 @@ const SauceCarousel = ({ sauceData }) => {
       <div className='container-fluid' >  
       <Carousel>
       <Carousel.Item style={{'height':"300px"}} >  
-      {/* {sauceData.fields.Name} */}
         <Carousel.Caption>  
-          <h3>First Demo </h3>  
+          {sauceData.map((records) => (
+              <h3>{records.Name}</h3> 
+            ))}
             </Carousel.Caption>  
             </Carousel.Item  >  
             <Carousel.Item style={{'height':"300px"}}>  
