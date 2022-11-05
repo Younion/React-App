f2b33e42447cb903c9cbbc9bbdd15dd7977603c1
Younion <106282337+Younion@users.noreply.github.com>
2022-11-04 21:24:39 -0600

    Add files via upload

--- App.js
+++ App.js
@@ -13,83 +13,59 @@ import Footer from "./Components/Footer";
 function App() {
 
   const [sauces, setSauces] = useState([]);
-  const [allSauces, setAllUsers] = useState([]);
+  const [names, setNames] = useState([]);
+  const [allSauces, setAllSauces] = useState([]);
 
   useEffect(() => {
-      const url = "endpoint";
-      const REACT_APP_API_KEY = "key";
+      const url = "https://api.airtable.com/v0/app4Kq78nyR93DHLC/hot%20sauces?filterByFormula=NOT({Average+Rating}+%3D+%27%27)&api_key=";
+      const REACT_APP_API_KEY = "key5DK4VYamAJtEoK";
       fetch(url + REACT_APP_API_KEY)
         .then(response => {
           return response.json();
         })
         .then(sauceData => {
-          sauceData.records.forEach((record) => {
-            // console.log(record.fields.Name);
-            setSauces(record.fields.Name);
-          })
+          setSauces(sauceData.records)
+          setAllSauces(sauceData.records)
+          setNames(sauceData.records)
         });
-    }, []);
+      })
+ 
+  const filterCards = event => {
+    const value = event.target.value.toLowerCase();
+    const filteredData = allSauces.filter(
+      sauces => (`${sauces.fields.Name} 
+      ${sauces['Heat Sources String']} 
+      ${"Average Rating: " + Math.round(sauces.fields['Average Rating'])} 
+      ${"Spiciness Rating: " + Math.round(sauces.fields['Spiciness Dots'])} 
+      ${"Percent Loved: " + Math.round(sauces.fields['Percent Loved'])*100 + "%"}`
+      .toLowerCase().includes(value)));
+      setSauces(filteredData);
+  }
 
   return (
     <div className="App">
-      <SauceCarousel key={sauces.id} sauceData={sauces}/>
+      <NavBar />
+      <TitleSection />
+      {names.map(name => 
+        <SauceCarousel key={name.id} sauceName={name.fields} />
+      )}
+      <CallToAction />
+      <div className="container-fluid" id="cta">
+        <h1>Hot Sauce Reviews</h1>
+      </div>
+      <div className="searchBar">
+      <input className="search" placeholder="Search..." onInput={filterCards}/>
+      </div>
+      <div className="cards-container">
+      {sauces.map(sauce => 
+        <SauceCard key={sauce.id} sauceData={sauce.fields} />
+      )}
+      </div>
+      <ReviewForm />
+      <AddForm />
+      <Footer />
     </div>
   );
 }
-  
-
-  // useEffect(() => {
-  //   (async () => {
-  //     let sauceData;
-  //     try {
-  //       const url = "https://api.airtable.com/v0/app4Kq78nyR93DHLC/hot%20sauces?filterByFormula=NOT({Average+Rating}+%3D+%27%27)&api_key=";
-  //       const REACT_APP_API_KEY = "keyjRGeqc5QA99Q5v";
-  //       const response = await fetch(url + REACT_APP_API_KEY);        
-  //       sauceData = await response.json();
-  //     } catch (error) {
-  //       console.log(error);
-  //       sauceData = [];
-  //     }
-  //     setSauces(sauceData.records);
-  //     setAllUsers(sauceData.records);
-  //     console.log(sauceData.records)
-  //   })();
-  // }, []);
- 
-  // const filterCards = event => {
-  //   const value = event.target.value.toLowerCase();
-  //   const filteredData = allSauces.filter(
-  //     sauces => (`${sauces.fields.Name} 
-  //     ${sauces.fields['Heat Sources String']} 
-  //     ${"Average Rating: " + Math.round(sauces.fields['Average Rating'])} 
-  //     ${"Spiciness Rating: " + Math.round(sauces.fields['Spiciness Dots'])} 
-  //     ${"Percent Loved: " + Math.round(sauces.fields['Percent Loved'])*100 + "%"}`
-  //     .toLowerCase().includes(value)));
-  //     setSauces(filteredData);
-  // }
-
-  // return (
-    // <div className="App">
-    //   <NavBar />
-    //   <TitleSection />
-    //   <SauceCarousel sauceData={sauces}/>
-    //   <CallToAction />
-    //   <div className="container-fluid" id="cta">
-    //     <h1>Hot Sauce Reviews</h1>
-    //   </div>
-    //   <div className="searchBar">
-    //   <input className="search" placeholder="Search..." onInput={filterCards}/>
-    //   </div>
-    //   <div className="cards-container">
-    //   {sauces.map((sauce, index) => (
-    //     <SauceCard key={sauce.id} sauceData={sauce} />
-    //     ))}
-    //   </div>
-    //   <ReviewForm />
-    //   <AddForm />
-    //   <Footer />
-    // </div>
-//   );
-// }
   
 export default App;
