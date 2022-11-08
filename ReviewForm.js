98f6ec3178e19b86422ae1582a1eba667c004810
Younion <106282337+Younion@users.noreply.github.com>
2022-11-07 17:35:01 -0700

    Update ReviewForm.js

--- ReviewForm.js
+++ ReviewForm.js
@@ -53,7 +53,7 @@ function ReviewForm() {
         }
 
         var Airtable = require('airtable');
-        var base = new Airtable({apiKey: 'key5DK4VYamAJtEoK'}).base('app4Kq78nyR93DHLC');
+        var base = new Airtable({apiKey: 'key'}).base('base');
 
         base('Tasting Form').create([formSubmit
         ], { typecast: true }, function(err, records) {
@@ -65,19 +65,6 @@ function ReviewForm() {
             console.log(record.getId());
           });
         });
-
-      //   fetch('https://api.airtable.com/v0/app4Kq78nyR93DHLC/Tasting%20Form', {
-      //     Authorization: "key5DK4VYamAJtEoK",
-      //     method: 'POST',
-      //     headers: { 
-      //       "Content-Type": "application/json"
-      //     },
-      //     body: JSON.stringify(formSubmit)
-      //   })
-      //   .then(() => {
-      //     // console.log("Post successful")
-      //   })
-      // console.log(formSubmit);
   }
 
   return (
